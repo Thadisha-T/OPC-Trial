6463d29bcf5acac8f9dd82c1d1c6dafbda0a30bf
Thadisha-T <tthi0007@student.monash.edu>
2023-04-22 23:28:01 +1000

    Obtain data in test client

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace HelloWorld
+﻿using System;
+using System.Threading.Tasks;
+using Workstation.ServiceModel.Ua;
+using Workstation.ServiceModel.Ua.Channels;
+					
+public class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
+    public static async Task Main() {
+        // Description of client application
+        var clientDescription = new ApplicationDescription {
+            ApplicationName = "Workstation.UaClient.FeatureTests",
+            ApplicationUri = $"urn:{System.Net.Dns.GetHostName()}:Workstation.UaClient.FeatureTests",
+            ApplicationType = ApplicationType.Client
+        };
+
+        // Create a client-side channel
+        var channel = new ClientSessionChannel(
+            clientDescription,
+            null,
+            new AnonymousIdentity(),
+            "opc.tcp://localhost:4840",
+            SecurityPolicyUris.None
+        );
+
+        try {
+            await channel.OpenAsync();
+            await channel.CloseAsync();
+        }
+
+        catch (Exception e) {
+            await channel.AbortAsync();
+            Console.WriteLine(e.Message);
         }
     }
 }
--- opc_test.py
+++ opc_test.py
@@ -1,23 +1,30 @@
 import asyncio
-from asyncua import Client, Node, ua 
-import logging
-from turtle import delay
-import time
-
-
-logging.basicConfig(level=logging.INFO)
-_logger = logging.getLogger('asyncua')
-
+from asyncua import Client
 
 async def main():
     # OPC UA Inputs
     URL_OPC_UA = "opc.tcp://localhost:4840"
-    print("TEST")
 
     async with Client(url = URL_OPC_UA) as client:
-        # Initalise OPCUA Variables
-        index = await client.get_namespace_index('OPC_UA_TEST_SERVER')
-    
+
+        # Initalise OPC UA Variables
+        NODE_TEMP = client.get_node("ns=2;i=2")
+
+        # Program Loop
+        while True:
+            # Polling Rate
+            await asyncio.sleep(2)
+
+            # Read Data
+            temp_data = await NODE_TEMP.read_value()
+            print("THE VALUE OF THE DATA = " + str(temp_data) + " DEGREES")
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     asyncio.run(main())
